4bf06541a60ccb78c008bfabc119e5a489ab90bf
FlutterDev <Keicy6778@gmail.com>
2020-10-12 22:50:44 +0100

    2020-10-12

--- lib/keicy_fcm_for_mobile_7_0.dart
+++ lib/keicy_fcm_for_mobile_7_0.dart
@@ -47,7 +47,7 @@ class KeicyFCMForMobile {
   }
 
   static Future<dynamic> _myBackgroundMessageHandler(Map<String, dynamic> message) async {
-    _instance.controller.add(message);
+    _instance._controller.add(message);
     if (message.containsKey('data')) {
       final dynamic data = message['data'];
     } else if (message.containsKey('notification')) {
